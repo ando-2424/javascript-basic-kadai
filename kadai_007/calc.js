5ef6fafcc5286838b913f37474ca57f96d922fe2
ando-2424 <yu.a.0713.24@gmail.com>
2024-04-03 23:51:10 +0900

    kadai_oo7再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -5,14 +5,14 @@ let num = Math.floor(Math.random() * 14);
 console.log(num);
 
 // 変数numの値によって、出力する文字列を切り替える
-if (num === 3 || num === 6 || num === 9 || num === 12) {
-    console.log('3の倍数です');
-}
-else if (num === 5 || num === 10) {
-  console.log('5の倍数です');
-}
-else if (num === 15) {
+if (num % 3 === 0 && num % 5 === 0 ) {
   console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
 }
 else {
   console.log(num);
